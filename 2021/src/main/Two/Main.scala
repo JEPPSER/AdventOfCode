dac9f6ddf9e2839b7514b4fc46f5a758a1cc4491
Jesper Bergström <jesper.b96@gmail.com>
2021-12-11 11:53:50 +0100

    Solves day 2 of 2021

---
+++ 2021/src/main/Two/Main.scala
@@ -0,0 +1,23 @@
+package main.Two
+
+import scala.io.Source
+
+object Main extends App {
+  val filename = "src/main/two/input.txt"
+  val values = Source.fromFile(filename).getLines.toArray
+  println(moveSubmarine(0, 0, 0, values))
+
+  def moveSubmarine(x: Int, y: Int, index: Int, values: Array[String]): Int = {
+    if (index >= values.length) x * y
+    else {
+      val parts = values(index).split(" ")
+      val steps = parts(1).toInt
+      moveSubmarine(
+        if (parts(0) == "forward") x + steps else x,
+        if (parts(0) == "up") y - steps else if (parts(0) == "down") y + steps else y,
+        index + 1,
+        values
+      )
+    }
+  }
+}
