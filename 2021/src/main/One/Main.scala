3ef22fe078790b475c8ec2698d42bd514f7e876b
Jesper Bergström <jesper.b96@gmail.com>
2021-12-11 11:21:34 +0100

    Solves day 1 of 2021

---
+++ 2021/src/main/One/Main.scala
@@ -0,0 +1,14 @@
+package main.One
+
+import scala.io.Source
+
+object Main extends App {
+  val filename = "src/main/one/input.txt"
+  val values = Source.fromFile(filename).getLines.toArray.map((s: String) => s.toInt)
+  println(checkIncrease(1, 0, values))
+
+  def checkIncrease(index: Int, count: Int, values: Array[Int]): Int = {
+    if (index >= values.length) count
+    else checkIncrease(index + 1, if (values(index) > values(index - 1)) count + 1 else count, values)
+  }
+}
